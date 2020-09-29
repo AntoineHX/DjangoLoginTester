ebeaf71976fbf7400e9bc906720d4c914ffbd5e7
Antoine Harle <antoine.harle@etu.upmc.fr>
2020-09-29 20:45:27 +0200

    Clean (sans auto-update)

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # DjangoLoginTester
 
+# Requis
+pip install django-crispy-forms
+
+# Utilisation
+Connexion utilisateur :
+- Username : admin
+- Password : admin
+
+# Liens
 https://code.visualstudio.com/docs/python/tutorial-django#_create-a-project-environment-for-the-django-tutorial
 
 https://learndjango.com/tutorials/django-login-and-logout-tutorial
@@ -8,10 +17,8 @@ https://djangoforbeginners.com/pages-app/
 
 Modal :
 https://dmorgan.info/posts/django-views-bootstrap-modals/
-pip install django-crispy-forms
 
 https://pypi.org/project/django-bootstrap-modal-forms/
-pip install django-bootstrap-modal-forms
 
 
 https://djangocentral.com/django-ajax-with-jquery/
--- loginTester/loginApp/views.py
+++ loginTester/loginApp/views.py
@@ -7,16 +7,23 @@ from django.shortcuts import render
 #     return HttpResponse("Hello, Django!")
 
 from django.views.generic import TemplateView
+from django.shortcuts import render
 # from django.template import loader
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
-    # def get(self, request, *args, **kwargs):
-    #     print('Home get:', request.POST)
-    #     context = {
-    #         'username': 'bip',
-    #     }
-    #     return self.render_to_response(context)
+    def get(self, request, *args, **kwargs):
+        # print(kwargs)
+        # print('Home get:', request.GET)
+        if request.is_ajax():
+            print('Ajax request')
+            # return return HttpResponseRedirect(reverse_lazy('home'))
+            return render(request, self.template_name)
+        context = {
+            'email': 'bip',
+        }
+        return self.render_to_response(context)
+        # return HttpResponseRedirect(reverse_lazy('home'))
     
     # def post(self, request, *args, **kwargs):
     #     print('Home post:', request.POST)
@@ -27,31 +34,17 @@ class HomePageView(TemplateView):
 
 from django.views.generic import FormView
 from django.urls import reverse_lazy
-# from django.contrib.messages.views import SuccessMessageMixin
-from django.http import JsonResponse
-
-from .forms import TestForm
-
-# class AjaxTemplateMixin(object):
-    
-#     def dispatch(self, request, *args, **kwargs):
-#         print('Ajax:',request.POST)
-#         if not hasattr(self, 'ajax_template_name'):
-#             split = self.template_name.split('.html')
-#             split[-1] = '_inner'
-#             split.append('.html')
-#             self.ajax_template_name = ''.join(split)
-#         if request.is_ajax():
-#             self.template_name = self.ajax_template_name
-#         return super(AjaxTemplateMixin, self).dispatch(request, *args, **kwargs)
 
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect,JsonResponse
+
+from .forms import TestForm
 
 class TestFormView(FormView):
     template_name = 'test_form.html'
     form_class = TestForm
     success_url = reverse_lazy('home')
-    # success_message = "Way to go!"
+    # success_message = ""
 
     def dispatch(self, request, *args, **kwargs):
         # print('Ajax:',request.POST)
@@ -70,12 +63,15 @@ class TestFormView(FormView):
         If the form is valid return HTTP 200 status 
         code along with name of the user
         """
-        # print('Form valid :', form)
-        # print(self.user)
+        # Mail update
         email = form.cleaned_data['email']
         update=User.objects.filter(username=self.user).update(email=email)
         assert update==1 #Un seul mail modifier
-        return JsonResponse({"email": email}, status=200)
+
+        response = HttpResponseRedirect(self.success_url)
+        #response = JsonResponse({"email": email}, status=200)
+        return response
+
     
     def form_invalid(self, form):
         """
